c2b324a2b4e55d16363fad366c217c05d9926a4c
Regina Khalil <khal4400@mylaurier.ca>
2020-07-26 20:46:12 -0400

    Update Assignment_4.c
    All functions initialized

--- Assignment_4.c
+++ Assignment_4.c
@@ -6,14 +6,24 @@
 
 void* requestResource(void*);  //function to request resource
 int releaseResource(int*);    //function to release resrouce
+int isSafe(); 				//safe algorithm 
+void bankerRun(); 			//Runs the banker algorithm 
+void printResources(); 		//print function 
+int init(int, char**); 		//initialize arrays and variables
+void updateNeed(int); 		//update need variable
+int allocate(int, int*); 	//allocare resource
+int isComplete(int); 		//Check for end process
+int isFinsihed(int*);		//Check if process is finished
 
 //Data structures 
 int* available;    //the available amount of each resource
 int** maximum;	   //the max demancd of each customer
 int** allocation;  //the amount currently allocated to each customer
 int** need; 	   //the remaining need of each customer
-
-
+int resource; 
+int process; 
+int* finish; 
+int* safeseq; 
 
 //synchronization mutex
 pthread_mutex_t mutexAvailable;
@@ -29,3 +39,56 @@ int main(int argc, char* argv[]) {
         return -1;
 	}
 }
+
+int init(int argc, char** argv){
+
+}
+
+void* requestResource(void* proc_in) {
+
+}
+
+int releaseResource(int* Request) {
+
+}
+
+int isSafe() {
+
+}
+
+void bankerRun() {
+
+}
+
+void printResources() {
+
+}
+
+void updateNeed(int process) {
+
+}
+int allocate(int process, int* Request){
+
+}
+int isComplete(int process){
+
+}
+int isFinsihed(int* fin) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
